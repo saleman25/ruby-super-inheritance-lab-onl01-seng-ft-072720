a697483fe6ac8f7da81b8cf72c0a028abbe5c865
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 05:27:38 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,11 +1,11 @@
 class ChattyStudent < Student 
   
- def hello
-   super 
-   "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
- end 
+def hello
+  super 
+  "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+end 
   
-  def raise_hand
+def raise_hand
     super 
     super
     super
@@ -16,6 +16,6 @@ class ChattyStudent < Student
     super
     super
     super
-  end 
+end 
   
 end  
